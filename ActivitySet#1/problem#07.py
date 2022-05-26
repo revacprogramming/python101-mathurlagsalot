bf038846b8cd7bdac90cc568d5d8502a097728fb
Apoorv Mathur <94664942+mathurlagsalot@users.noreply.github.com>
2022-05-26 09:27:35 +0000

    p7 fix

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,8 +1,17 @@
-# Strings
+#strings
 
+def myinput():
+    text = "X-DSPAM-Confidence:    0.8475"
+    return text
 
-str = "X-DSPAM-Confidence:    0.8475"
-stringind = str.find("0.8475")
-ans = str[stringind::1]
-res = float(ans)
-print(res)
+def convert(text):
+    Pos = text.find(':')
+    last = text[Pos+1:]
+    end = float(last)
+    print(end)
+
+def main():
+    text=myinput()
+    convert(text)
+
+main()
