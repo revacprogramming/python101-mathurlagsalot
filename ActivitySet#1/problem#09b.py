d9c9a87536c4c3a048e8aadf5ef6e7274aa28b8a
Apoorv Mathur <94664942+mathurlagsalot@users.noreply.github.com>
2022-06-30 15:09:29 +0530

    Update problem#09b.py

--- ActivitySet#1/problem#09b.py
+++ ActivitySet#1/problem#09b.py
@@ -1,13 +1,16 @@
-fop = open("mbox-short.txt")
-c = 0
-for l in fop:
-    l = l.rstrip()
-    if l == "": continue
-        
-    words = l.split()
-    if words[0] !="From": continue
-        
-    print(words[1])
-    c += 1
+def words():
+  fop = open("mbox-short.txt")
+  c = 0
+  for l in fop:
+      l = l.rstrip()
+      if l == "": continue
+          
+      words = l.split()
+      if words[0] !="From": continue
+          
+      print(words[1])
+      c += 1
+  
+  print ("There were", c, "lines in the file with From as the first word")
 
-print ("There were", c, "lines in the file with From as the first word")
+words()
