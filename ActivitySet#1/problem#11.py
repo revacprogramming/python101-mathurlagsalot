336916a521bd16c854b51e5c2f19c708edbc91d2
Apoorv Mathur <94664942+mathurlagsalot@users.noreply.github.com>
2022-06-30 15:10:30 +0530

    Update problem#11.py

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,20 +1,22 @@
 # Tuples
+def file():
+  filename = "dataset/mbox-short.txt"
+  filehandle=open(filename)
+  counts={}
+  
+  for line in filehandle:
+  	if line.startswith("From "):
+  		time=line.split()[5].split(":")
+  		counts [time[0]] = counts.get(time[0], 0) + 1
+  
+  
+  lst = list()
+  
+  for key, value in counts.items():
+      lst.append( (key,value) )
+  lst.sort()
+  
+  for hour, counts in lst:
+      print (hour, counts)
 
-filename = "dataset/mbox-short.txt"
-filehandle=open(filename)
-counts={}
-
-for line in filehandle:
-	if line.startswith("From "):
-		time=line.split()[5].split(":")
-		counts [time[0]] = counts.get(time[0], 0) + 1
-
-
-lst = list()
-
-for key, value in counts.items():
-    lst.append( (key,value) )
-lst.sort()
-
-for hour, counts in lst:
-    print (hour, counts)
+file()
