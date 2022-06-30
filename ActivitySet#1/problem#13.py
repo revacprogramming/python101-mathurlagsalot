c3bf9f7bcc2f92fc4ea670791256ffaed1ce8929
Apoorv Mathur <94664942+mathurlagsalot@users.noreply.github.com>
2022-06-30 15:13:27 +0530

    Update problem#13.py

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -2,14 +2,17 @@
 # https://www.py4e.com/lessons/network
 
 import socket
-mysock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org',80))
-cmd='GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+def networkprog():
+  mysock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  mysock.connect(('data.pr4e.org',80))
+  cmd='GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+  mysock.send(cmd)
+  
+  while True:
+      data=mysock.recv(512)
+      if(len(data)<1):
+          break
+      print(data.decode())
+  mysock.close()
 
-while True:
-    data=mysock.recv(512)
-    if(len(data)<1):
-        break
-    print(data.decode())
-mysock.close()
+networkprog()
