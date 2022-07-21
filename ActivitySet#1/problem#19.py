dbccf5c382ff1bc10bb9de7ae10e965cf1061419
Apoorv Mathur <94664942+mathurlagsalot@users.noreply.github.com>
2022-07-21 15:02:49 +0530

    Update problem#19.py

---
+++ ActivitySet#1/problem#19.py
@@ -0,0 +1,17 @@
+fname = input("Enter file name: ")
+
+if len(fname) < 1 : 
+    fname = "mbox-short.txt"
+    
+    
+fh = open(fname)
+count = 0
+
+for line in fh:
+    if line.startswith('From '):
+        line = line.split()
+        line = line[1]
+        print(line)
+        count += 1
+
+print("There were", count, "lines in the file with From as the first word")
